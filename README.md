9f1d9bb0fde43eaa47830801cc6ca5d91c3f85be
antono <121531680+antonobekasi@users.noreply.github.com>
2025-06-12 10:40:27 +0700

    new update

--- README.md
+++ README.md
@@ -1,13 +1,54 @@
-# Responsive Chips Website
-## [Watch it on youtube](https://youtu.be/vcb-cEkE-UI)
-### Responsive Chips Website
+<div align="center">
+  
+  ![GitHub repo size](https://img.shields.io/github/repo-size/adnan-bhaldar/anime-changing-scenery)
+  ![GitHub stars](https://img.shields.io/github/stars/adnan-bhaldar/Anime-Changing-Scenery?style=social)
+  ![GitHub forks](https://img.shields.io/github/forks/adnan-bhaldar/Anime-Changing-Scenery?style=social)
+[![Twitter Follow](https://img.shields.io/twitter/follow/Adnan__Bhaldar_?style=social)](https://twitter.com/intent/follow?screen_name=Adnan__Bhaldar)
 
-- Responsive Chips Website Design Using HTML CSS And JavaScript
-- Contains animations when scrolling.
-- Smooth scrolling in each section.
-- Developed first with the Mobile First methodology, then for desktop.
-- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+  <br />
+  <br />
+  
+  <img src="./Desktop.png" />
+  <h2 align="center">Anime Changing Scenery</h2>
 
+  Anime Changing Scenery is a fully responsive Fun website page, <br />Responsive for all devices, built using HTML, CSS, and JavaScript.
 
+   <a href="https://adnan-bhaldar.github.io/Anime-Changing-Scenery/"><strong>➥ Live Demo</strong></a>
 
-![preview img](/preview.png)
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Anime Changing Scenery](./Desktop2.png "Desktop Demo")
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+* [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+### Run Locally
+
+To run **Anime Changing Scenery** locally, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+sudo git clone https://github.com/adnan-bhaldar/Anime-Changing-Scenery.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/adnan-bhaldar/Anime-Changing-Scenery.git
+```
+
+### Contact
+
+If you want to contact with me you can reach me at [Twitter](https://www.twitter.com/Adnan__Bhaldar).
+
+### License
+
+This project is **free to use** and does not contains any license.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,138 @@
-/*=============== SHOW MENU ===============*/
+var rotateDiv = document.getElementById('rot');
+var rotateIcons = document.getElementById('rot-icons');
+var clickRotateDiv = document.getElementById('click-rot');
+var angle = 0;
 
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+clickRotateDiv.onclick = function() {
+  angle += 60;
+  rotateDiv.style.transform = 'rotate(' + angle + 'deg)';
+  rotateIcons.style.transform = 'rotate(' + angle + 'deg)';
+};
 
-      /* Menu Show */
-if(navToggle){
-    navToggle.addEventListener('click' , () =>{
-        navMenu.classList.add('show-menu')
-    }
-    
-    )
-}      
-      /* Menu hidden */
+var step = 2;
+var color1 = 'rgba(0,0,0,0.5)';
+var color2 = 'rgba(0,0,0,0.1)';
 
-if(navClose){
-    navClose.addEventListener('click' , () =>{
-        navMenu.classList.remove('show-menu')
-    }
-    )
-}      
-
-/*=============== REMOVE MENU MOBILE ===============*/
-
-const navLink = document.querySelectorAll('nav__link')
-
-const linkAction = ()=>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+var gradient = ' conic-gradient(';
+for (var i = 0; i < 360; i += step) {
+  var color = i % (2 * step) === 0 ? color1 : color2;
+  gradient += color + ' ' + i + 'deg, ';
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+gradient = gradient.slice(0, -2) + '), rgb(85 93 108)'; 
+
+rotateDiv.style.background = gradient;
 
 
-/*=============== SHADOW HEADER ===============*/
-const scrollHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll',shadowHeader)
+var toggles = document.querySelectorAll('.toggle');
+var tempElement = document.querySelector('.temp');
 
+let isAnimating = false; // Add flag to indicate if animation is active
 
-/*=============== SWIPER FAVORITES ===============*/ 
-const swiperFavourites = new Swiper('.favourites__swiper', {
-   
-    loop: true,
-    grabCursor: true,
-    slidesPerView: 'auto',
-    centeredSlides: 'auto',
-  
-   
+toggles.forEach(function(toggle) {
+  toggle.addEventListener('click', function() {
+    if (this.classList.contains('active') || isAnimating) { // Check if animation is active
+      return;
+    }
+    toggles.forEach(function(toggle) {
+      toggle.classList.remove('active');
+    });
+    this.classList.add('active');
+    var tempValue = parseFloat(tempElement.textContent);
+    if (this.id === 'toggle-cel') {
+      var celsius = Math.round((tempValue - 32) * 5 / 9);
+      tempElement.textContent = celsius + '°C';
+    } else if (this.id === 'toggle-far') {
+      var fahrenheit = Math.round(tempValue * 9 / 5 + 32);
+      tempElement.textContent = fahrenheit + '°F';
+    }
   });
+});
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up')
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+let currentTempF = 34; // Initialize with the initial temperature in Fahrenheit
+
+// cubic ease in/out function
+function easeInOutCubic(t) {
+  return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
 }
-window.addEventListener('scroll' , scrollUp)
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+function changeTemp(element, newTemp) {
+  let unit = element.innerHTML.includes("F") ? "°F" : "°C";
+  let currentTemp = unit === "°F" ? currentTempF : Math.round((currentTempF - 32) * 5 / 9);
+  let finalTemp = unit === "°F" ? newTemp : Math.round((newTemp - 32) * 5 / 9);
+
+  let duration = 2000; // Duration of the animation in milliseconds
+  let startTime = null;
+
+  function animate(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+
+    let elapsed = currentTime - startTime;
+    let progress = Math.min(elapsed / duration, 1);
+    progress = easeInOutCubic(progress);
+
+    let tempNow = Math.round(currentTemp + (progress * (finalTemp - currentTemp)));
+    element.innerHTML = `${tempNow}${unit}`;
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      // Update currentTempF once the animation is complete
+      currentTempF = newTemp;
+      isAnimating = false; // Reset the flag when animation is done
+    }
+  }
+
+  isAnimating = true; // Set flag when animation starts
+  requestAnimationFrame(animate);
+}
 
 
+window.onload = function() {
+  const sixths = Array.from(document.querySelectorAll('.sixths'));
+  let index = 0;
+  let temp = document.querySelector('.temp');
 
+  document.querySelector('#rot-icons').addEventListener('click', () => {
+    sixths[index].classList.remove('active');
+    index = (index + 1) % sixths.length;
+    sixths[index].classList.add('active');
+    if (index == 0 ) {
+      changeTemp(temp, 34);
+      console.log("sun")
+      document.querySelector('#mountains').classList.remove("snow");
+      document.querySelector('#mountains').classList.remove("clouds");
+    } else if (index == 1) {
+      changeTemp(temp, 27);
+      console.log("sunset")
+      document.querySelector('#mountains').classList.add("sunset");
+    } else if (index == 2) {
+      changeTemp(temp, 14);
+      console.log("moon")
+      document.querySelector('#mountains').classList.remove("sunset");
+      document.querySelector('#mountains').classList.add("moon");
+    } else if (index == 3) {
+      changeTemp(temp, 16);
+      console.log("clouds")
+      document.querySelector('#mountains').classList.add("clouds");
+    } else if (index == 4) {
+      changeTemp(temp, 8);
+      console.log("storm")
+      document.querySelector('#mountains').classList.add("storm");
+    } else if (index == 5) {
+      changeTemp(temp, -4);
+      console.log("snow")
+      document.querySelector('#mountains').classList.remove("moon");
+      document.querySelector('#mountains').classList.remove("storm");
+      document.querySelector('#mountains').classList.add("snow");
+    }
 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration:2500,
-    delay:300,
-})
-
-sr.reveal(`.home__data`)
-sr.reveal(`.home__circle, .home__img` , {delay:600, scale:.5})
-sr.reveal(`.home__chips-1, .home__chips-2 , .home__chips-3` , {delay:1000, interval:100})
+    let loadingBar = document.querySelector('.loading-bar');
+    loadingBar.classList.add('active');
+  
+    setTimeout(() => {
+      loadingBar.classList.remove('active');
+    }, 1200);
+  });
+};
